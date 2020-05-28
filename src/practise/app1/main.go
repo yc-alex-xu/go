26522f239477f7e05753d43482270f94e502c6b3
Alex Xu <xuyc@sina.com>
2020-05-28 17:06:33 +0800

    modified:   app1/main.go

--- src/practise/app1/main.go
+++ src/practise/app1/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-	"practise/math"
 	"fmt"
+	"practise/math"
+)
+
+const (
+	i      = 100
+	pi     = 3.1415
+	prefix = "Go_"
+)
+
+var (
+	num_g    float64
+	isActive bool // 全局变量声明
+
 )
 
 func main() {
-	fmt.Printf("Hello, world.  Sqrt(2) = %v\n", math.Sqrt(2))
+	num := 2.0
+	num_g = num
+	fmt.Printf("你好，Sqrt(%v) = %v\n", num_g, math.Sqrt(num_g))
 }
