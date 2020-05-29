36334cfcc3207640e3d9b97097547e8a543f622d
Alex Xu <xuyc@sina.com>
2020-05-29 17:41:28 +0800

    modified:   main.go

--- src/practise/channels/main.go
+++ src/practise/channels/main.go
@@ -1,23 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func sum(a []int, name string, c chan int) {
 	total := 0
 	for _, v := range a {
 		total += v
 	}
-	fmt.Println(name, "send back sum")
-	c <- total // send total to c
+	c <- total
+	fmt.Println(name, "end")
+
 }
 
 func main() {
 	a := []int{7, 2, 8, -9, 4, 0} //slice
 	fmt.Println("the slice is:", a)
-	chan2sum := make(chan int)                     //定义一个channel时，也需要定义发送到channel的值的类型
-	go sum(a[:len(a)/2], "the 1st half", chan2sum) //goroutine被调度的次序不太确定
-	go sum(a[len(a)/2:], "the 2nd half", chan2sum)
-	x, y := <-chan2sum, <-chan2sum // receive from channel
-
-	fmt.Println("one  half=", x, "the another half=", y)
+	chan2sum := make(chan int)                 //定义一个channel
+	go sum(a[:len(a)/2], "1st", chan2sum) //go_1
+	x := <-chan2sum
+	go sum(a[len(a)/2:], "2nd", chan2sum) //go_2
+	//如果go_2和go_1 放一起，到时谁先被blocked中被唤醒就不确定了
+	fmt.Println("sum of 1/2 slice =", x)
+	y := <-chan2sum // receive from channel
+	fmt.Println("sum of another 1/2 slice=", y)
+	time.Sleep(time.Duration(1) * time.Second)
 }
