6d894b53bf6d6c37ca6269019dd275eeac5efdad
Alex Xu <xuyc@sina.com>
2020-05-29 10:59:04 +0800

    deleted:    build.sh 	modified:   main.go

--- src/practise/app1/main.go
+++ src/practise/app1/main.go
@@ -12,13 +12,12 @@ const (
 )
 
 var (
-	num_g    float64
+	num_g    = 2
 	isActive bool // 全局变量声明
 
 )
 
 func main() {
-	num := 2.0
-	num_g = num
-	fmt.Printf("你好，Sqrt(%v) = %v\n", num_g, math.Sqrt(num_g))
+	num := float64(num_g)
+	fmt.Printf("你好，Sqrt(%v) = %v\n", num_g, math.Sqrt(num))
 }
