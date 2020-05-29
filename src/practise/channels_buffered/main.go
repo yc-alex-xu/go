558345eedbe5592d151e50f9a239104c2df55a1f
Alex Xu <xuyc@sina.com>
2020-05-29 15:29:09 +0800

    new file:   main.go

---
+++ src/practise/channels_buffered/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func sum(a []int, name string, c chan int) {
+	total := 0
+	for _, v := range a {
+		total += v
+	}
+	fmt.Println(name, "send back sum")
+	c <- total // send total to c
+}
+
+func main() {
+	a := []int{7, 2, 8, -9, 4, 0} //slice
+	fmt.Println("the slice is:", a)
+	chan2sum := make(chan int, 3)                  //指定channel buffer size 为3
+	go sum(a[:len(a)/2], "the 1st half", chan2sum) //goroutine被调度的次序不太确定
+	go sum(a[len(a)/2:], "the 2nd half", chan2sum)
+	x, y := <-chan2sum, <-chan2sum // receive from channel
+
+	fmt.Println("one  half=", x, "the another half=", y)
+}
