28df3c40fa11358e779ee93402bac1d356e93195
Alex Xu <xuyc@sina.com>
2020-06-01 13:22:16 +0800

    modified:   ../src/practise/database/ORM.go 	modified:   ../src/practise/database/export.sh 	modified:   ../src/practise/database/mysql.go

--- src/practise/database/ORM.go
+++ src/practise/database/ORM.go
@@ -3,54 +3,95 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql" // 导入数据库驱动
 )
 
+type Userinfo struct { //表名，field名字，在数据库中实际都是小写
+	Uid        int `orm:"PK"` //如果表的主键不是id，那么需要加上pk注释，显式的说这个字段是主键
+	Username   string
+	Departname string
+	Created    time.Time
+}
 type User struct {
-	ID   int
-	Name string `orm:"size(100)"`
+	Uid     int `orm:"PK"` //如果表的主键不是id，那么需要加上pk注释，显式的说这个字段是主键
+	Name    string
+	Profile *Profile `orm:"rel(one)"`      // OneToOne relation
+	Post    []*Post  `orm:"reverse(many)"` // 设置一对多的反向关系
 }
 
+type Profile struct {
+	Id   int
+	Age  int16
+	User *User `orm:"reverse(one)"` // 设置一对一反向关系(可选)
+}
+
+type Post struct {
+	Id    int
+	Title string
+	User  *User  `orm:"rel(fk)"`
+	Tags  []*Tag `orm:"rel(m2m)"` //设置一对多关系
+}
+
+type Tag struct {
+	Id    int
+	Name  string
+	Posts []*Post `orm:"reverse(many)"`
+}
+
+/* after init
+mysql> show tables;
++----------------+
+| Tables_in_test |
++----------------+
+| post           |
+| post_tags      |
+| profile        |
+| tag            |
+| user           |
+| userdetail     |
+| userinfo       |
++----------------+
+*/
+
 func init() {
+	fmt.Println("ORM init start")
+	orm.Debug = true
 	// 设置默认数据库
 	orm.RegisterDataBase("default", "mysql", "root:"+os.Getenv("MySQL_PASSWORD")+"@tcp("+os.Getenv("MySQL_HOST")+":3306)/test?charset=utf8", 30)
 
-	// 注册定义的 model
-	orm.RegisterModel(new(User))
-	//RegisterModel 也可以同时注册多个 model
-	//orm.RegisterModel(new(User), new(Profile), new(Post))
-
-	/* 创建 table user
-	mysql> show tables;
-	+----------------+
-	| Tables_in_test |
-	+----------------+
-	| user           |
-
-	mysql> drop table user;
-
-	*/
-	orm.RunSyncdb("default", false, true)
+	orm.RegisterModel(new(Userinfo), new(User), new(Profile), new(Post), new(Tag))
+	orm.RunSyncdb("default", false, true) //create table
+	fmt.Println("ORM init end")
 }
 
 func main() {
 	o := orm.NewOrm()
+	var Uid int
+	var u Userinfo
+	for _, s := range []string{"smith", "john", "kate", "mary"} {
+		u.Username = s
+		u.Departname = "tbd"
+		fmt.Println(s)
+		id, err := o.Insert(&u)
+		if err != nil {
+			panic(err)
+		} else {
+			Uid = int(id)
+			fmt.Printf("inserted: (%d, %v)\n", id, s)
+		}
 
-	user := User{Name: "slene"}
-
-	// 插入 table user
-	id, err := o.Insert(&user)
-	fmt.Printf("ID: %d, ERR: %v\n", id, err)
+	}
 
 	// 更新表
-	user.Name = "John"
-	num, err := o.Update(&user)
+	u.Username = "new"
+	num, err := o.Update(&u)
 	fmt.Printf("NUM: %d, ERR: %v\n", num, err)
 
 	// 读取 one
-	u := User{ID: user.ID}
+	u = Userinfo{Uid: Uid}
 	err = o.Read(&u)
 	fmt.Printf("ERR: %v\n", err)
 
--- src/practise/database/mysql.go
+++ src/practise/database/mysql.go
@@ -13,34 +13,38 @@ func main() {
 
 	db, err := sql.Open("mysql", "root:"+os.Getenv("MySQL_PASSWORD")+"@tcp("+os.Getenv("MySQL_HOST")+":3306)/test?charset=utf8")
 	checkErr(err)
+	defer db.Close()
 
 	//插入数据
 	stmt, err := db.Prepare("INSERT INTO userinfo SET username=?,department=?,created=?")
 	checkErr(err)
 
-	res, err := stmt.Exec("John", "研发部门", "2012-12-09")
+	res, err := stmt.Exec("john", "研发", "2012-12-09")
+	res, err = stmt.Exec("smith", "财务", "2018-12-09")
+	res, err = stmt.Exec("mary", "人事", "2020-12-09")
+	res, err = stmt.Exec("kate", "销售", "2020-1-09")
 	checkErr(err)
 
 	id, err := res.LastInsertId()
 	checkErr(err)
 
-	fmt.Println(id)
+	fmt.Println("LastInsertId():", id)
 	//更新数据
 	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
 	checkErr(err)
 
-	res, err = stmt.Exec("Johnupdate", id)
+	res, err = stmt.Exec("new", id)
 	checkErr(err)
 
 	affect, err := res.RowsAffected()
 	checkErr(err)
 
-	fmt.Println(affect)
+	fmt.Println("row affect:", affect)
 
 	//查询数据
 	rows, err := db.Query("SELECT * FROM userinfo")
 	checkErr(err)
-
+	fmt.Println("SELECT * FROM userinfo")
 	for rows.Next() {
 		var uid int
 		var username string
@@ -48,26 +52,19 @@ func main() {
 		var created string
 		err = rows.Scan(&uid, &username, &department, &created)
 		checkErr(err)
-		fmt.Println(uid)
-		fmt.Println(username)
-		fmt.Println(department)
-		fmt.Println(created)
+		fmt.Println(uid, username, department, created)
 	}
 
 	//删除数据
-	stmt, err = db.Prepare("delete from userinfo where uid=?")
+	stmt, err = db.Prepare("delete from userinfo")
 	checkErr(err)
 
-	res, err = stmt.Exec(id)
+	res, err = stmt.Exec()
 	checkErr(err)
 
 	affect, err = res.RowsAffected()
 	checkErr(err)
-
-	fmt.Println(affect)
-
-	db.Close()
-
+	fmt.Println("row affect:", affect)
 }
 
 func checkErr(err error) {
