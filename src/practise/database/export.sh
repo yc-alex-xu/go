23c249aee454d10f180fba5759c806609804393d
Alex Xu <xuyc@sina.com>
2020-06-01 15:29:06 +0800

    modified:   export.sh 	new file:   redis.go

--- src/practise/database/export.sh
+++ src/practise/database/export.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export MySQL_HOST=10.152.183.135
 export MySQL_PASSWORD=password
+export Redis_HOST=10.152.183.202
---
+++ src/practise/database/redis.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+var (
+	Pool *redis.Pool
+)
+
+func init() {
+	Pool = newPool(os.Getenv("Redis_HOST") + ":6379")
+	close()
+}
+
+func newPool(server string) *redis.Pool {
+
+	return &redis.Pool{
+
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func close() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	signal.Notify(c, syscall.SIGKILL)
+	go func() {
+		<-c
+		Pool.Close()
+		os.Exit(0)
+	}()
+}
+
+func main() {
+	conn := Pool.Get()
+	defer conn.Close()
+	key := "name"
+	if data, err := redis.Bytes(conn.Do("GET", key)); err != nil {
+		fmt.Printf("error get %s: %v \n", key, err)
+	} else {
+		fmt.Println("get ", key, ":", string(data))
+	}
+
+}
