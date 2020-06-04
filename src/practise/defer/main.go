53b7004ec6beac28dbbf868564c74a014dfdc456
Alex Xu <xuyc@sina.com>
2020-06-04 19:57:21 +0800

    modified:   ../defer/main.go

--- src/practise/defer/main.go
+++ src/practise/defer/main.go
@@ -1,11 +1,31 @@
+/*
+defer 对普通函数与闭包的影响
+
+Go functions may be closures. A closure is a function value that references variables from outside its body.
+The function may access and assign to the referenced variables; in this sense the function is "bound" to the variables.
+
+
+*/
 package main
 
 import "fmt"
 
 func main() {
+	f()
 	for i := 0; i < 5; i++ {
 		defer fmt.Println(i)
-		defer func(n int) { fmt.Println("lamda=>", n) }(i)
+		//a lambda function as closure
+		defer func(n int) {
+			fmt.Println("lamda=>", n*2)
+		}(i) //
 	}
+}
 
+func f() {
+	for i := 0; i < 4; i++ {
+		//function与context是联系在一起，所以每一次循环都是一个新函数
+		g := func(i int) { fmt.Printf("%d ", i) }
+		g(i)
+		fmt.Printf(" - g is of type %T and has value %v\n", g, g)
+	}
 }
