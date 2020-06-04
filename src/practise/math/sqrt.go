2cbe42cbd7fe9ea0ff32c9a2b7f431d0f57ac330
Alex Xu <xuyc@sina.com>
2020-06-04 15:19:12 +0800

    modified:   ../math/sqrt.go

--- src/practise/math/sqrt.go
+++ src/practise/math/sqrt.go
@@ -1,9 +1,11 @@
 package math
 
-func Sqrt(x float64) float64 {
-	z := 0.0
+/*
+Sqrt (x float64)  return the square root of x
+*/
+func Sqrt(x float64) (z float64) {
 	for i := 0; i < 1000; i++ {
 		z -= (z*z - x) / (2 * x)
 	}
-	return z
+	return
 }
