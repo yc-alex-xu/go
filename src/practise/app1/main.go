a6a34843edf7092cdc884d88e5318ec1e88ac303
Alex Xu <xuyc@sina.com>
2020-06-04 10:32:35 +0800

    modified:   app1/main.go

--- src/practise/app1/main.go
+++ src/practise/app1/main.go
@@ -12,23 +12,34 @@ const (
 )
 
 var (
-	num_g    = 2
+	numG     = 2
 	isActive bool // 全局变量声明
 
 )
 
 func main() {
-	num := float64(num_g)
+	num := float64(numG)
 	//` 括起的字符串为Raw字符串，即字符串在代码中的形式就是打印时的形式，它没有字符转义，换行也将原样输出。
 	s := `Hi
 		你
 		好
 		world
 		`
-	fmt.Printf("Sqrt(%v) = %v\n", num_g, math.Sqrt(num))
+	fmt.Printf("Sqrt(%v) = %v\n", numG, math.Sqrt(num))
 	a := []byte(s)
 	b := a
 	a[0] = 'h'
 	fmt.Println(string(b))
-
+	m := make(map[int]string)
+	m[0] = "John"
+	m[1] = "Mary"
+	fmt.Println(m)
+	m2 := map[string]string{
+		"+86": "China",
+		"+1":  "North America",
+	}
+	fmt.Println(m2["+86"])
+	for k, v := range m2 {
+		fmt.Println(k, v)
+	}
 }
