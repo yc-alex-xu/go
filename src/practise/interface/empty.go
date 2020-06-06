d1726ad2388d9dfd91c6a62523a7dc790c8adb36
Alex Xu <xuyc@sina.com>
2020-06-06 16:16:25 +0800

    modified:   empty.go 	deleted:    type.go

--- src/practise/interface/empty.go
+++ src/practise/interface/empty.go
@@ -1,6 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+//List  type: slice of Element
+type List []interface{} //slice
+
+type person struct {
+	name string
+	age  int
+}
+
+func (p person) String() string {
+	return "(name: " + p.name + " - age: " + strconv.Itoa(p.age) + " years)"
+}
 
 func main() {
 	var e interface{}
@@ -10,6 +25,8 @@ func main() {
 	fmt.Printf("e: %v \n", e) //可以自动判别element
 	e = s
 	fmt.Printf("e: %v \n", e) //可以自动判别element
+
+	fmt.Print("e: ")
 	/*
 		value, ok = e.(T) 语法
 	*/
@@ -20,5 +37,28 @@ func main() {
 	} else {
 		fmt.Printf("is of a different type\n")
 
+	}
+	interfaceType()
+}
+
+/*.(type) 用法
+ */
+func interfaceType() {
+	l := make(List, 3) //make a slice
+	l[0] = 1           //an int
+	l[1] = "Hello"     //a string
+	l[2] = person{"Dennis", 70}
+
+	for index, element := range l {
+		switch value := element.(type) {
+		case int:
+			fmt.Printf("l[%d] is an int and its value is %d\n", index, value)
+		case string:
+			fmt.Printf("l[%d] is a string and its value is %s\n", index, value)
+		case person:
+			fmt.Printf("l[%d] is a person and its value is %s\n", index, value)
+		default:
+			fmt.Println("l[%d] is of a different type", index)
+		}
 	}
 }
