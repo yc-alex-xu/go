c8c69762630878c37fde256f41dc16c8987ee8f9
Alex Xu <xuyc@sina.com>
2020-06-06 14:43:22 +0800

    modified:   anonym.go 	modified:   main.go 	modified:   override.go

--- src/practise/struct/main.go
+++ src/practise/struct/main.go
@@ -10,13 +10,13 @@ type person struct {
 	age  int
 }
 
-// 比较两个人的年龄，返回年龄大的那个人，并且返回年龄差
-// struct也是传值的
-func Older(p1, p2 person) (person, int) {
+// older 比较两个人的年龄，返回年龄大的那个人
+func older(p1, p2 person) person {
 	if p1.age > p2.age { // 比较p1和p2这两个人的年龄
-		return p1, p1.age - p2.age
+		return p1
 	}
-	return p2, p2.age - p1.age
+	return p2
+
 }
 
 func main() {
@@ -31,16 +31,8 @@ func main() {
 	// 按照struct定义顺序初始化值
 	paul := person{"Paul", 43}
 
-	tb_Older, tb_diff := Older(tom, bob)
-	tp_Older, tp_diff := Older(tom, paul)
-	bp_Older, bp_diff := Older(bob, paul)
+	senior := older(older(tom, bob), paul)
+	fmt.Printf("%s is the oldest as age %d \n",
+		senior.name, senior.age)
 
-	fmt.Printf("Of %s and %s, %s is older by %d years\n",
-		tom.name, bob.name, tb_Older.name, tb_diff)
-
-	fmt.Printf("Of %s and %s, %s is older by %d years\n",
-		tom.name, paul.name, tp_Older.name, tp_diff)
-
-	fmt.Printf("Of %s and %s, %s is older by %d years\n",
-		bob.name, paul.name, bp_Older.name, bp_diff)
 }
--- src/practise/struct/override.go
+++ src/practise/struct/override.go
@@ -2,22 +2,21 @@ package main
 
 import "fmt"
 
-type Human struct {
-	name string
-	age int
-	phone string  // Human类型拥有的字段
+type human struct {
+	name  string
+	age   int
+	phone string // human类型拥有的字段
 }
 
-type Employee struct {
-	Human  // 匿名字段Human
+type employee struct {
+	human      // 匿名字段human
 	speciality string
-	phone string  // 雇员的phone字段
+	phone      string // 雇员的phone字段
 }
 
 func main() {
-	Bob := Employee{Human{"Bob", 34, "777-444-XXXX"}, "Designer", "333-222"}
+	Bob := employee{human{"Bob", 34, "777-444-XXXX"}, "Designer", "333-222"}
 	fmt.Println("Bob's work phone is:", Bob.phone)
-	// 如果我们要访问Human的phone字段
-	fmt.Println("Bob's personal phone is:", Bob.Human.phone)
+	// 访问human的phone字段
+	fmt.Println("Bob's personal phone is:", Bob.human.phone)
 }
-
