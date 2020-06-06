12de0a0fbb8a98bb2af6467c454b7f525b06c4d5
Alex Xu <xuyc@sina.com>
2020-06-06 17:27:42 +0800

    modified:   main.go

--- src/practise/goroutine/main.go
+++ src/practise/goroutine/main.go
@@ -14,6 +14,19 @@ func say(name string) {
 }
 
 func main() {
-	go say("world") //开一个新的Goroutines执行
-	say("hello")    //当前Goroutines执行
+	fmt.Println("cpu number: ", runtime.NumCPU())
+	go say("world") //g1:开一个新的Goroutines执行
+	say("hello")    //g2:当前Goroutines执行
+	/*即使是g1,g2相互谦让，如果没有下一句的话，也是不能保证g1都能执行完
+	$ go run main.go
+	cpu number:  4
+	goroutine: hello loop: 0
+	goroutine: world loop: 0
+	goroutine: hello loop: 1
+	goroutine: world loop: 1
+	goroutine: hello loop: 2
+	goroutine: hello loop: 3
+	goroutine: hello loop: 4
+	*/
+	fmt.Println("goodbye")
 }
