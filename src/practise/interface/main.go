dbdbaa2567335e2f45e4d09cc175b4d2d9480398
Alex Xu <xuyc@sina.com>
2020-06-06 15:26:47 +0800

    modified:   main.go

--- src/practise/interface/main.go
+++ src/practise/interface/main.go
@@ -2,75 +2,62 @@ package main
 
 import "fmt"
 
-type Human struct {
-	name string
-	age int
+type human struct {
+	name  string
+	age   int
 	phone string
 }
 
-type Student struct {
-	Human //匿名字段
+type student struct {
+	human  //匿名字段
 	school string
-	loan float32
+	loan   float32
 }
 
-type Employee struct {
-	Human //匿名字段
+type employee struct {
+	human   //匿名字段
 	company string
-	money float32
+	money   float32
 }
 
-//Human实现SayHi方法
-func (h Human) SayHi() {
+//human实现SayHi方法
+func (h human) SayHi() {
 	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
 
-//Human实现Sing方法
-func (h Human) Sing(lyrics string) {
+//human实现Sing方法
+func (h human) Sing(lyrics string) {
 	fmt.Println("La la la la...", lyrics)
 }
 
-//Employee重载Human的SayHi方法
-func (e Employee) SayHi() {
+//employee重载human的SayHi方法
+func (e employee) SayHi() {
 	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name,
 		e.company, e.phone)
-	}
+}
 
-// Interface Men被Human,Student和Employee实现
+// Interface men被human,student和employee实现
 // 因为这三个类型都实现了这两个方法
-type Men interface {
+type men interface {
 	SayHi()
 	Sing(lyrics string)
 }
 
 func main() {
-	mike := Student{Human{"Mike", 25, "222-222-XXX"}, "MIT", 0.00}
-	paul := Student{Human{"Paul", 26, "111-222-XXX"}, "Harvard", 100}
-	sam := Employee{Human{"Sam", 36, "444-222-XXX"}, "Golang Inc.", 1000}
-	tom := Employee{Human{"Tom", 37, "222-444-XXX"}, "Things Ltd.", 5000}
+	mike := student{human{"Mike", 25, "222-222-XXX"}, "MIT", 0.00}
+	paul := student{human{"Paul", 26, "111-222-XXX"}, "Harvard", 100}
+	sam := employee{human{"Sam", 36, "444-222-XXX"}, "Golang Inc.", 1000}
+	tom := employee{human{"Tom", 37, "222-444-XXX"}, "Things Ltd.", 5000}
 
-	//定义Men类型的变量i
-	var i Men
-
-	//i能存储Student
-	i = mike
-	fmt.Println("This is Mike, a Student:")
-	i.SayHi()
-	i.Sing("November rain")
-
-	//i也能存储Employee
-	i = tom
-	fmt.Println("This is tom, an Employee:")
-	i.SayHi()
-	i.Sing("Born to be wild")
-
-	//定义了slice Men
-	fmt.Println("Let's use a slice of Men and see what happens")
-	x := make([]Men, 3)
+	//定义了slice men
+	fmt.Println("Let's use a slice of men and see what happens")
+	x := make([]men, 4)
 	//这三个都是不同类型的元素，但是他们实现了interface同一个接口
-	x[0], x[1], x[2] = paul, sam, mike
+	x[0], x[1], x[2], x[3] = paul, sam, mike, tom
 
-	for _, value := range x{
-		value.SayHi()
+	songs := []string{"November rain", "Born to be wild"}
+	for k, v := range x {
+		v.SayHi()
+		v.Sing(songs[k%len(songs)]) //顺序选歌
 	}
 }
