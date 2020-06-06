4534e68dd2472ef9c5abfb7f4821b71e29b799b2
Alex Xu <xuyc@sina.com>
2020-06-06 14:24:16 +0800

    modified:   main.go

--- src/practise/env/main.go
+++ src/practise/env/main.go
@@ -19,26 +19,15 @@ import (
 )
 
 func main() {
-
-	//os.Clearenv()
-
-	os.Setenv("FOO", "1")
-
-	fmt.Println("FOO:", os.Getenv("FOO"))
-
-	os.Unsetenv("FOO")
-
-	fmt.Println("FOO:", os.Getenv("FOO"))
-
-	os.Setenv("hello", "1")
-
-	os.Setenv("world", "2")
-
-	fmt.Println()
 	// fetcha all env variables as shown in $ env
 	for _, element := range os.Environ() {
 		variable := strings.Split(element, "=")
 		fmt.Println(variable[0], "=>", variable[1])
 	}
+	os.Clearenv() //只影响本process
+	os.Setenv("FOO", "1")
+	fmt.Println("FOO:", os.Getenv("FOO"))
+	os.Unsetenv("FOO")
+	fmt.Println("FOO:", os.Getenv("FOO"))
 
 }
