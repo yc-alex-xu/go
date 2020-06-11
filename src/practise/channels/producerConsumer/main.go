ba3636beb7709952e41be190ad265cf39111a335
Alex Xu <xuyc@sina.com>
2020-06-11 16:04:38 +0800

    modified:   channels/producerConsumer/main.go

--- src/practise/channels/producerConsumer/main.go
+++ src/practise/channels/producerConsumer/main.go
@@ -4,27 +4,36 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
 var done = make(chan bool)
-var msgs = make(chan int)
+var quit = make(chan bool)
+var ch = make(chan int)
 
 func produce() {
 	for i := 0; i < 10; i++ {
-		msgs <- i
+		ch <- i
+		runtime.Gosched()
 	}
 	done <- true
 }
 
 func consume() {
 	for {
-		msg := <-msgs
-		fmt.Println(msg)
+		select {
+		case i := <-ch:
+			fmt.Println(i)
+			runtime.Gosched()
+		case <-done:
+			quit <- true
+			break
+		}
 	}
 }
 
 func main() {
 	go produce()
 	go consume()
-	<-done
+	<-quit
 }
