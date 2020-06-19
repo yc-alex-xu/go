dd0f8fd24ef99e2a169ae02ee1f89fb7ab843f98
Alex Xu <xuyc@sina.com>
2020-06-19 15:32:23 +0800

    modified:   defer/main.go

--- src/practise/defer/main.go
+++ src/practise/defer/main.go
@@ -7,11 +7,36 @@ import (
 	"fmt"
 )
 
-func main() {
+func deferSimple() {
 	for i := 0; i < 5; i++ {
 		defer fmt.Println("normal func=>", i)
+		defer func() {
+			fmt.Println("lamda func loop variable =>", i)
+		}()
 		defer func(n int) {
-			fmt.Println("lamda func=>", n*2)
+			fmt.Println("lamda func param=>", n)
 		}(i) //
 	}
+
+}
+
+func deferComplicated() {
+	a := 1
+	b := 2
+	//calc("10", a, b) 并不会被defer
+	defer calc("1", a, calc("10", a, b))
+	a = 0
+	defer calc("2", a, calc("20", a, b))
+	b = 1
+}
+
+func calc(index string, a, b int) int {
+	ret := a + b
+	fmt.Println(index, a, b, ret)
+	return ret
+}
+
+func main() {
+	deferSimple()
+	deferComplicated()
 }
