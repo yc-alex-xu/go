ba28cc1ff968936db23d3751dece16157e08af8c
Alex Xu <xuyc@sina.com>
2020-06-20 08:29:53 +0800

    deleted:    practise/goroutine/goodbye/main.go 	modified:   practise/goroutine/main.go 	deleted:    practise/goroutine/mutex/main.go 	deleted:    practise/goroutine/mutexSync/main.go 	deleted:    practise/goroutine/sync/main.go

--- src/practise/goroutine/main.go
+++ src/practise/goroutine/main.go
@@ -13,20 +13,20 @@ func say(name string) {
 	}
 }
 
+/*
+$ go run main.go
+cpu number:  4
+goroutine: hello loop: 0
+goroutine: world loop: 0
+goroutine: hello loop: 1
+goroutine: hello loop: 2
+goroutine: hello loop: 3
+goroutine: hello loop: 4
+goodbye
+*/
 func main() {
 	fmt.Println("cpu number: ", runtime.NumCPU())
-	go say("world") //g1:开一个新的Goroutines执行
-	say("hello")    //g2:当前Goroutines执行
-	/*即使是g1,g2相互谦让，如果没有下一句的话，也是不能保证g1都能执行完
-	$ go run main.go
-	cpu number:  4
-	goroutine: hello loop: 0
-	goroutine: world loop: 0
-	goroutine: hello loop: 1
-	goroutine: world loop: 1
-	goroutine: hello loop: 2
-	goroutine: hello loop: 3
-	goroutine: hello loop: 4
-	*/
+	go say("world")
+	say("hello")
 	fmt.Println("goodbye")
 }
