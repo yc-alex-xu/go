08dbe3671d8b0dc0df43c3d893e70a03c39290bd
Alex Xu <xuyc@sina.com>
2020-08-13 21:06:10 +0800

    new file:   leetcode/001/README.md 	new file:   leetcode/001/main.go

---
+++ src/leetcode/001/main.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+	dict := make(map[int]int)
+	for i,v := range nums{
+			dict[v]= i
+	}
+	for i,v := range nums{
+			if j,ok := dict[target-v]; ok && j!=i{//found
+					return []int{i,j}
+			}
+	}
+	return nil
+}
