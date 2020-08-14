69b7d3b87318a6560705304778934f07a474ee45
Alex Xu <xuyc@sina.com>
2020-08-14 11:01:41 +0800

    modified:   001/main.go 	new file:   002/README.md 	new file:   002/main.go

--- src/leetcode/001/main.go
+++ src/leetcode/001/main.go
@@ -1,12 +1,22 @@
+package main
+
+import "fmt"
+
 func twoSum(nums []int, target int) []int {
 	dict := make(map[int]int)
-	for i,v := range nums{
-			dict[v]= i
+	for i, v := range nums {
+		dict[v] = i
 	}
-	for i,v := range nums{
-			if j,ok := dict[target-v]; ok && j!=i{//found
-					return []int{i,j}
-			}
+	for i, v := range nums {
+		if j, ok := dict[target-v]; ok && j != i { //found
+			return []int{i, j}
+		}
 	}
 	return nil
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	fmt.Println(twoSum(nums, target))
 }
---
+++ src/leetcode/002/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+//ListNode the linked list node
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	c := 0 //carry
+	head, p := (*ListNode)(nil), (*ListNode)(nil)
+	for ; l1 != nil || l2 != nil || c != 0; l1, l2 = next(l1), next(l2) {
+		a, b := val(l1), val(l2)
+		t := new(ListNode)
+		t.Val = (a + b + c) % 10
+		t.Next = nil
+		c = (a + b + c) / 10
+		if p == nil { //first node
+			p, head = t, t
+		} else {
+			p.Next = t
+			p = t
+		}
+	}
+	return head
+}
+
+func next(p *ListNode) *ListNode {
+	if p == nil {
+		return nil
+	}
+	return p.Next
+
+}
+
+func val(p *ListNode) int {
+	if p == nil {
+		return 0
+	}
+	return p.Val
+}
+
+func main() {
+	l1 := &ListNode{Val: 3, Next: nil}
+	l1 = &ListNode{Val: 4, Next: l1}
+	l1 = &ListNode{Val: 2, Next: l1}
+
+	l2 := &ListNode{Val: 4, Next: nil}
+	l2 = &ListNode{Val: 6, Next: l2}
+	l2 = &ListNode{Val: 5, Next: l2}
+
+	l := addTwoNumbers(l1, l2)
+	fmt.Println(l, l.Next, l.Next.Next)
+
+}
