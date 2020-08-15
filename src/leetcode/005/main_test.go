beae7209a08b6879939de83fce12ae9044ba6a22
Alex Xu <xuyc@sina.com>
2020-08-15 16:43:35 +0800

    new file:   005/README.md 	new file:   005/main.go 	new file:   005/main_test.go

---
+++ src/leetcode/005/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "tc1",
+			args: args{s: "babad"},
+			want: "bab",
+		},
+		{
+			name: "tc2",
+			args: args{s: "cbbd"},
+			want: "bb",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
